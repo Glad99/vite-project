a436c507c2a09791692a5f13f220eea502cd5bb5
Gladys <gladysalu1@gmail.com>
2024-01-18 14:36:04 +0100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,19 @@
 const Footer = () => {
     return(
-        <footer className="flex justify-between bg-black text-white mt-10 gap-20 pt-5 ">
-            <div className="flex">
-                <div className="bg-white size-20 flex " >
+        <footer className="flex bg-black text-white mt-10 gap-20 p-5">
+            <div className="flex mr-20 ml-5">
+                <div className="bg-white size-20 flex" >
                     <img src="src/assets/Vector.png" className="px-1 py-1 " alt="" />
                 </div>
-                <div>
+                <div className="ml-2 border border-dashed rounded px-2 py-1 ">
                     <p>Scan to download on the playstore and Appstore. </p>
-                    <div>
+                    <div className="flex">
                     <img src="src/assets/ion_logo-google-playstore.svg" alt="" />
                     <img src="src/assets/Vector (2).svg" alt="" />
                     </div>
                 </div>
             </div>
-            <div className="flex" >
+            <div className="flex gap-20 ml-20 " >
                 <div>
                     <h1>About us</h1>
                     <p>Our company</p>
